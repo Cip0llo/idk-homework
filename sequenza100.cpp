7df815dbe2b91b89ed91870d088f4639bc7a60a2
Cip0llo <89394465+Cip0llo@users.noreply.github.com>
2025-02-26 10:38:07 +0100

    :3

--- sequenza100.cpp
+++ sequenza100.cpp
@@ -5,11 +5,11 @@ float input,sommaInput;
 int totInput=0;
 
 int main(){
-    do{
+    while(sommaInput<100){
         cout<<"Dai un numero: \n";
         cin>>input;
         sommaInput+=input;
         totInput++;
-    }while(sommaInput<100);
+    }
     cout<<"La somma totale è di: "<<sommaInput<<endl<<"La quantità di numeri inseriti è: "<<totInput<<endl;
 }
