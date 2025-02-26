9ddb19af7b85489ddf27420dc7921892194403a7
Cip0llo <89394465+Cip0llo@users.noreply.github.com>
2025-02-26 11:07:50 +0100

    :3

---
+++ differenzeNumeriSuccessivi.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int n;
+float cur_input,old_input,delta,deltaConst,deltaMax;
+
+int main(){
+    while(n<=0){
+        cout<<"Inserire quante ripetizioni (numero intero e positivo):\n",
+                cin>>n;
+    }
+    while(cur_input<=old_input){
+        cout<<"Inserire un numero:\n";
+        cin>>cur_input;
+    }
+    for(n=n-1;n>0;n--){
+        old_input=cur_input;
+        do{
+            cout<<"Inserire un numero più grande:\n";
+            cin>>cur_input;
+        }while(cur_input<=old_input);    
+        delta=cur_input-old_input;
+        if(delta==deltaConst){
+            cout<<"La differenza è di: "<<deltaConst<<endl;
+        }else{
+            if(delta>deltaMax){
+                deltaMax=delta;
+            }cout<<"La differenza massima è di: "<<deltaMax<<endl;
+        }
+        deltaConst=delta;
+    }
+    return 0;
+}
