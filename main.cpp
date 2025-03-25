af8954112c8cf9318d17e1a8b9967791f610016c
Cip0llo <89394465+Cip0llo@users.noreply.github.com>
2025-03-25 14:10:00 +0100

    :3
    :3

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int n,operazione;
+int main(){
+    cout<<"Ciao, dammi un numero tra 1 e 20:\n";
+    do{cin>>n;}while(n<=0||n>=10);
+    if(n%%2==0){for(int i=10;i>0;i--){mult+=n;cout<<mult<<endl;}}
+    else{
+
+    }
+
+
+
+
+
+}
