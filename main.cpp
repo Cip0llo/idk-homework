728c84ed836331639de679f74b54939962630049
Cip0llo <89394465+Cip0llo@users.noreply.github.com>
2025-04-08 12:11:49 +0200

    :3
    :3

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int n,operazione;
+int n,m,somma;
 int main(){
     cout<<"Ciao, dammi un numero tra 1 e 20:\n";
     do{cin>>n;}while(n<=0||n>=10);
-    if(n%%2==0){for(int i=10;i>0;i--){mult+=n;cout<<mult<<endl;}}
-    else{
-
-    }
-
-
-
-
-
+    if(n%%2==0){for(int i=10;i>0;i--){
+        m++;n*m;somma=n*m+somma;
+        cout<<n<<endl;
+    }}
+    else{for(int i=10;i>0;i--){
+        n++;somma+=n;
+        cout<<n<<endl;
+    }}
+    cout<<"la somma finale e:"<<somma;
 }
